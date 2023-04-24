61b86bb0f7624e8d6cde0c0b692b4aa7493a9f45
Powei Amughan <Poweiamughan@gmail.com>
2023-04-24 15:58:44 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('/n');
+	return (o);
+}
