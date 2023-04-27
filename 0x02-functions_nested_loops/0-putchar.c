8d8c6b5ac34a08d08d8e11d0e75e37234c919610
Powei Amughan <Poweiamughan@gmail.com>
2023-04-26 19:33:53 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Put char program
+ * Return: 0
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
