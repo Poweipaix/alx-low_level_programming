6e18d365a6319615843a87dfbd9e4bfbe7334748
Powei Amughan <Poweiamughan@gmail.com>
2023-05-08 11:07:45 -0700

    Task 0 fills memory with constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _memset - Entry point
+ * _memset - Fills the first n bytes of memory
  * @s: Pointer destination
  * @b: constant byte
  * @n: bytes
- * Return: s
+ * Return: 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] b;
+		s[i] = b;
 	return (s);
 }
