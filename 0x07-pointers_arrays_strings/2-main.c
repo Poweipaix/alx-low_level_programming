93e34caa3e2014937b0cf54851a8a9d57e499826
Powei Amughan <Poweiamughan@gmail.com>
2023-05-08 21:56:33 -0700

    Task 2 locates a character in string

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strchr.c -o 2-strchr
+julien@ubuntu:~/0x07$ ./2-strchr 
+llo
